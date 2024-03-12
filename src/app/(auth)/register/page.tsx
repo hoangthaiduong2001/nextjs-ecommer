42ddb3651a86fb855d7148f884feeb4d967d94cd
hoangthaiduong2001 <19521407@gm.uit.edu.vn>
2024-03-12 07:06:57 +0700

    update code

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,12 @@
+import { ModeToggle } from "@/components/toggle-theme";
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <ModeToggle />
+      Register page
+    </div>
+  );
+};
+
+export default RegisterPage;
